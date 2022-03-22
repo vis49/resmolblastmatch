136b0cd5fc6cc116cdcb3edee75929ac5729c2ea
vis49 <102168986+vis49@users.noreply.github.com>
2022-03-22 11:42:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print('''To use this program run a blast2 with the fasta of your protein that is modeled in jmol as the query sequence
 with your original protein as the subject. Copy paste the entire "in the middle" matching amino acid row and input it 
 as the match string. Then find the number of the first matching amino acid of your query sequence and input that as the
-number of the first match. You should then get the residue numbers of your matching amino acids to be put into resmol.''')
+number of the first match. You should then get the residue numbers of your matching amino acids to be put into jmol.''')
 print()
 pure_matches = input('Input match string:')
 
